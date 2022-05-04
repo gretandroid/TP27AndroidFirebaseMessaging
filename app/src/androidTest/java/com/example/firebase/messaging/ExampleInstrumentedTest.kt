f445e15830404787b26b23c05beb6bbbb3b196ff
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-04 10:25:57 +0200

    on y go johny go

--- app/src/androidTest/java/com/example/firebase/messaging/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/firebase/messaging/ExampleInstrumentedTest.kt
@@ -11,20 +11,11 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.firebase.messaging", appContext.packageName)
-
-
         FirebaseMessaging.getInstance().token
             .addOnCompleteListener(OnCompleteListener { task ->
                 if (!task.isSuccessful) {
@@ -35,6 +26,7 @@ class ExampleInstrumentedTest {
                 val token = task.result
                 // On recupére et on affiche le token
                 Log.d("TOKEN", token)
+                assert(token.isNotBlank())
             })
     }
 }
