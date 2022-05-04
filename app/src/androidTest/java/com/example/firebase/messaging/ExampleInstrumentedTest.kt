eb2fff22d30a6332a99c77ca849ce4c7bccbd99c
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-04 10:15:41 +0200

    ajout de app/google-services.json a .gitignore

--- app/src/androidTest/java/com/example/firebase/messaging/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/firebase/messaging/ExampleInstrumentedTest.kt
@@ -1,7 +1,10 @@
 package com.example.firebase.messaging
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,5 +23,18 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.firebase.messaging", appContext.packageName)
+
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    println("La recuperation du token a echoue")
+                    return@OnCompleteListener
+                }
+                // On recupere le TOKEN
+                val token = task.result
+                // On recupére et on affiche le token
+                Log.d("TOKEN", token)
+            })
     }
 }
--- app/src/main/java/com/example/firebase/messaging/MainActivity.kt
+++ app/src/main/java/com/example/firebase/messaging/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.firebase.messaging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    println("La recuperation du token a echoue")
+                    return@OnCompleteListener
+                }
+                // On recupere le TOKEN
+                val token = task.result
+                // On recupére et on affiche le token
+                Log.d("TOKEN", token)
+            })
     }
 }
